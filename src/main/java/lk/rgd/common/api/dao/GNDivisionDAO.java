5e2de7fdcc5552629deceb19967b93e4fe7e5a4e
asankha <asankha@asankha>
2010-06-01 11:24:29 +0530

    add DSDivisions and GNDivisions and associated DAOs

---
+++ src/main/java/lk/rgd/common/api/dao/GNDivisionDAO.java
@@ -0,0 +1,30 @@
+package lk.rgd.common.api.dao;
+
+import lk.rgd.common.api.domain.GNDivision;
+
+import java.util.Map;
+
+/**
+ * @author asankha
+ */
+public interface GNDivisionDAO {
+
+    /**
+     * Returns the list of GNDivision names for the given language with the ID
+     *
+     * @param districtId the District ID
+     * @param dsDivisionId the DS Division ID
+     * @param language the language ID (see AppConstants)
+     * @return a Map of known G.N. Division names for the given language along with the ID
+     */
+    public Map<Integer, String> getGNDivisionNames(int districtId, int dsDivisionId, String language);
+
+    /**
+     * Return GNDivision by id
+     * @param districtId the District ID
+     * @param dsDivisionId the DS Division ID
+     * @param gnDivisionId the G.N. Division ID
+     * @return the GNDivision
+     */
+    public GNDivision getGNDivision(int districtId, int dsDivisionId, int gnDivisionId);
+}
