2ad83fb10ad7a3635dbf8d57be773ce5354f1ccc
duminda <duminda@localhost>
2010-06-09 18:07:00 +0530

    middle of user creation

--- src/main/java/lk/rgd/crs/web/action/UserManagmentAction.java
+++ src/main/java/lk/rgd/crs/web/action/UserManagmentAction.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
+import lk.rgd.common.api.domain.User;
+
 /**
  * @author amith jayasekara
  */
@@ -14,15 +16,37 @@ public class UserManagmentAction extends ActionSupport implements SessionAware {
 
     private static final Logger logger = LoggerFactory.getLogger(BirthRegisterAction.class);
     private Map session;
+    private User user = new User();
+    private int pageNo = 0;
 
 
     public String creatUser() {
-         logger.info("creat user called");
+        logger.info("creat user called");
+        if(pageNo == 1)  {
+            // to do the code.....
+            return "sucess";
+        }
         return "pageLoad";
     }
 
+    public User getUser() {
+        return user;
+    }
 
-    public void setSession(Map<String, Object> stringObjectMap) {
+    public void setUser(User user) {
+        this.user = user;
+    }
 
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public int getPageNo() {
+
+        return pageNo;
+    }
+    
+    public void setSession(Map map) {
+        this.session = map;
     }
 }
