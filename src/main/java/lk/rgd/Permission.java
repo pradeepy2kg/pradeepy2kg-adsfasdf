c11ff3125c11bf8c0f4e6036ff05dee4dd683b93
duminda <duminda@localhost>
2010-06-24 09:52:39 +0530

    fix user permissions used when generating menu

--- src/main/java/lk/rgd/Permission.java
+++ src/main/java/lk/rgd/Permission.java
@@ -12,16 +12,16 @@ public final class Permission {
     public static final int APPROVE_BDF_CONFIRMATION = 6;
 
     //permission for pages
-    public static final int CREATE_USER_PAGE = 25;
-    public static final int BIRTH_REGISTRATON_PAGE = 26;
-    public static final int BIRTH_CONFIRMATION_REPORT_PAGE = 27;
-    public static final int BIRTH_CONFIRMATION_PRINT_PAGE = 28;
-    public static final int BIRTH_CONFIRMATION_PAGE = 29;
-    public static final int BIRTH_REGISTRATION_APPROVAL_PAGE = 30;
-    public static final int USER_PREFERANCE_SELECT_PAGE = 31;
-    public static final int VIEW_USERS = 32;
-    public static final int BIRTH_CONFIRMATION_SEARCH = 33;
-    public static final int BIRTH_CONFIRMATION_APPROVAL_PAGE = 34;
+    public static final int PAGE_CREATE_USER = 25;
+    public static final int PAGE_BIRTH_REGISTRATON = 26;
+    public static final int PAGE_BIRTH_CONFIRMATION_REPORT = 27;
+    public static final int PAGE_BIRTH_CONFIRMATION_PRINT = 28;
+    public static final int PAGE_BIRTH_CONFIRMATION = 29;
+    public static final int PAGE_BIRTH_REGISTRATION_APPROVAL = 30;
+    public static final int PAGE_USER_PREFERANCE_SELECT = 31;
+    public static final int PAGE_VIEW_USERS = 32;
+    public static final int PAGE_BIRTH_CONFIRMATION_SEARCH = 33;
+    public static final int PAGE_BIRTH_CONFIRMATION_APPROVAL = 34;
 
     // permission for PRS
     public static final int PRS_ADD_PERSON  = 50;
