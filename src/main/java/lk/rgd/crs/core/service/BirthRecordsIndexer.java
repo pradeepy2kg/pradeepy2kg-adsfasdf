45134ccb799fcdc6266b8f3370a77e94fb13541f
asankha <asankha@localhost>
2010-07-12 21:58:48 +0530

    make BDF IDUKey Long instead of long; add new birth records to index on BC issue

--- src/main/java/lk/rgd/crs/core/service/BirthRecordsIndexer.java
+++ src/main/java/lk/rgd/crs/core/service/BirthRecordsIndexer.java
@@ -27,6 +27,16 @@ public class BirthRecordsIndexer {
         this.birthDeclarationDAO = birthDeclarationDAO;
     }
 
+    public void add(BirthDeclaration bdf) {
+        try {
+            addRecord(bdf);
+            solrIndexManager.getServer().commit();
+            logger.info("Successfully indexed birth record : {}", bdf.getIdUKey());
+        } catch (Exception e) {
+            logger.error("Error indexing birth record : " + bdf.getIdUKey(), e);
+        }
+    }
+
     public void indexAll() {
 
         // delete all existing
@@ -41,74 +51,7 @@ public class BirthRecordsIndexer {
         int count = 0;
         try {
             for (BirthDeclaration bdf : bdfList) {
-
-                SolrInputDocument d = new SolrInputDocument();
-                d.addField("idUKey", bdf.getIdUKey());
-
-                // add registration info
-                BirthRegisterInfo regInfo = bdf.getRegister();
-                d.addField("bdfSerialNo", regInfo.getBdfSerialNo());
-                d.addField("dateOfRegistration", regInfo.getDateOfRegistration());
-                //d.addField("comments", regInfo.getComments());
-                d.addField("originalBCDateOfIssue", regInfo.getOriginalBCDateOfIssue());
-                //d.addField("originalBCPlaceOfIssue", regInfo.getOriginalBCPlaceOfIssue());
-                d.addField("birthDistrict",
-                    regInfo.getBirthDistrict().getEnDistrictName() + " / " +
-                        regInfo.getBirthDistrict().getSiDistrictName() + " / " +
-                        regInfo.getBirthDistrict().getTaDistrictName() + " / ");
-                d.addField("birthDivision",
-                    regInfo.getBirthDivision().getEnDivisionName() + " / " +
-                        regInfo.getBirthDivision().getSiDivisionName() + " / " +
-                        regInfo.getBirthDivision().getTaDivisionName() + " / ");
-
-                // add child details
-                ChildInfo child = bdf.getChild();
-                d.addField("childFullNameEnglish", child.getChildFullNameEnglish());
-                d.addField("childFullNameOfficialLang", child.getChildFullNameOfficialLang());
-                d.addField("dateOfBirth", child.getDateOfBirth());
-                d.addField("birthAtHospital", child.getBirthAtHospital());
-                d.addField("childGender", GenderUtil.getGenderString(child.getChildGender()));
-                d.addField("pin", child.getPin());
-                d.addField("placeOfBirth", child.getPlaceOfBirth());
-                //d.addField("numberOfChildrenBorn", child.getNumberOfChildrenBorn());
-
-                // add mother details
-                ParentInfo parent = bdf.getParent();
-                if (parent != null) {
-                    d.addField("motherFullName", parent.getMotherFullName());
-                    d.addField("motherAddress", parent.getMotherAddress());
-                    d.addField("motherDOB", parent.getMotherDOB());
-                    d.addField("motherNICorPIN", parent.getMotherNICorPIN());
-                    d.addField("motherPassportNo", parent.getMotherPassportNo());
-                    d.addField("motherPlaceOfBirth", parent.getMotherPlaceOfBirth());
-
-                    // add father details
-                    d.addField("fatherFullName", parent.getFatherFullName());
-                    d.addField("fatherDOB", parent.getFatherDOB());
-                    d.addField("fatherNICorPIN", parent.getFatherNICorPIN());
-                    d.addField("fatherPassportNo", parent.getFatherPassportNo());
-                    d.addField("fatherPlaceOfBirth", parent.getFatherPlaceOfBirth());
-                }
-
-                // add informant info
-                InformantInfo informant = bdf.getInformant();
-                d.addField("informantName", informant.getInformantName());
-                d.addField("informantAddress", informant.getInformantAddress());
-                d.addField("informantNICorPIN", informant.getInformantNICorPIN());
-
-                // add grandfather and great grand father info
-                GrandFatherInfo grandFatherInfo = bdf.getGrandFather();
-                if (grandFatherInfo != null) {
-                    d.addField("grandFatherFullName", grandFatherInfo.getGrandFatherFullName());
-                    d.addField("grandFatherBirthPlace", grandFatherInfo.getGrandFatherBirthPlace());
-                    d.addField("grandFatherBirthYear", grandFatherInfo.getGrandFatherBirthYear());
-
-                    d.addField("greatGrandFatherFullName", grandFatherInfo.getGreatGrandFatherFullName());
-                    d.addField("greatGrandFatherBirthPlace", grandFatherInfo.getGreatGrandFatherBirthPlace());
-                    d.addField("greatGrandFatherBirthYear", grandFatherInfo.getGreatGrandFatherBirthYear());
-                }
-
-                solrIndexManager.getServer().add(d);
+                addRecord(bdf);
                 count++;
             }
 
@@ -125,5 +68,75 @@ public class BirthRecordsIndexer {
         } catch (Exception e) {
             logger.error("Unexpected Exception encountered during re-indexing");
         }
+    }
+
+    private void addRecord(BirthDeclaration bdf) throws SolrServerException, IOException {
+        SolrInputDocument d = new SolrInputDocument();
+        d.addField("idUKey", bdf.getIdUKey());
+
+        // add registration info
+        BirthRegisterInfo regInfo = bdf.getRegister();
+        d.addField("bdfSerialNo", regInfo.getBdfSerialNo());
+        d.addField("dateOfRegistration", regInfo.getDateOfRegistration());
+        //d.addField("comments", regInfo.getComments());
+        //d.addField("originalBCDateOfIssue", regInfo.getOriginalBCDateOfIssue());
+        //d.addField("originalBCPlaceOfIssue", regInfo.getOriginalBCPlaceOfIssue());
+        d.addField("birthDistrict",
+            regInfo.getBirthDistrict().getEnDistrictName() + " / " +
+                regInfo.getBirthDistrict().getSiDistrictName() + " / " +
+                regInfo.getBirthDistrict().getTaDistrictName() + " / ");
+        d.addField("birthDivision",
+            regInfo.getBirthDivision().getEnDivisionName() + " / " +
+                regInfo.getBirthDivision().getSiDivisionName() + " / " +
+                regInfo.getBirthDivision().getTaDivisionName() + " / ");
+
+        // add child details
+        ChildInfo child = bdf.getChild();
+        d.addField("childFullNameEnglish", child.getChildFullNameEnglish());
+        d.addField("childFullNameOfficialLang", child.getChildFullNameOfficialLang());
+        d.addField("dateOfBirth", child.getDateOfBirth());
+        d.addField("birthAtHospital", child.getBirthAtHospital());
+        d.addField("childGender", GenderUtil.getGenderString(child.getChildGender()));
+        d.addField("pin", child.getPin());
+        d.addField("placeOfBirth", child.getPlaceOfBirth());
+        //d.addField("numberOfChildrenBorn", child.getNumberOfChildrenBorn());
+
+        // add mother details
+        ParentInfo parent = bdf.getParent();
+        if (parent != null) {
+            d.addField("motherFullName", parent.getMotherFullName());
+            d.addField("motherAddress", parent.getMotherAddress());
+            d.addField("motherDOB", parent.getMotherDOB());
+            d.addField("motherNICorPIN", parent.getMotherNICorPIN());
+            d.addField("motherPassportNo", parent.getMotherPassportNo());
+            d.addField("motherPlaceOfBirth", parent.getMotherPlaceOfBirth());
+
+            // add father details
+            d.addField("fatherFullName", parent.getFatherFullName());
+            d.addField("fatherDOB", parent.getFatherDOB());
+            d.addField("fatherNICorPIN", parent.getFatherNICorPIN());
+            d.addField("fatherPassportNo", parent.getFatherPassportNo());
+            d.addField("fatherPlaceOfBirth", parent.getFatherPlaceOfBirth());
+        }
+
+        // add informant info
+        InformantInfo informant = bdf.getInformant();
+        d.addField("informantName", informant.getInformantName());
+        d.addField("informantAddress", informant.getInformantAddress());
+        d.addField("informantNICorPIN", informant.getInformantNICorPIN());
+
+        // add grandfather and great grand father info
+        GrandFatherInfo grandFatherInfo = bdf.getGrandFather();
+        if (grandFatherInfo != null) {
+            d.addField("grandFatherFullName", grandFatherInfo.getGrandFatherFullName());
+            d.addField("grandFatherBirthPlace", grandFatherInfo.getGrandFatherBirthPlace());
+            d.addField("grandFatherBirthYear", grandFatherInfo.getGrandFatherBirthYear());
+
+            d.addField("greatGrandFatherFullName", grandFatherInfo.getGreatGrandFatherFullName());
+            d.addField("greatGrandFatherBirthPlace", grandFatherInfo.getGreatGrandFatherBirthPlace());
+            d.addField("greatGrandFatherBirthYear", grandFatherInfo.getGreatGrandFatherBirthYear());
+        }
+
+        solrIndexManager.getServer().add(d);
     }
 }
