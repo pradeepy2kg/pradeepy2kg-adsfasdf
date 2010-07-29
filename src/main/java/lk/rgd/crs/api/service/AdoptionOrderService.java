b3899160ce652068e7f754575e84bf86c76f121e
ashoka <ashoka@localhost>
2010-07-29 11:43:27 +0530

    new fields in AdoptionOrder bean

--- src/main/java/lk/rgd/crs/api/service/AdoptionOrderService.java
+++ src/main/java/lk/rgd/crs/api/service/AdoptionOrderService.java
@@ -1,6 +1,7 @@
 package lk.rgd.crs.api.service;
 
 import lk.rgd.crs.api.domain.AdoptionOrder;
+import lk.rgd.common.api.domain.User;
 
 import java.util.List;
 
@@ -8,13 +9,17 @@ import java.util.List;
  * @author Ashoka Ekanayaka
  */
 public interface AdoptionOrderService {
-    public AdoptionOrder getById(long adoptionId);
+    public AdoptionOrder getById(long adoptionId, User user);
 
-    public void addAdoptionOrder(AdoptionOrder adoption);
+    public void addAdoptionOrder(AdoptionOrder adoption, User user);
 
-    public void updateAdoptionOrder(AdoptionOrder bdf);
+    public void updateAdoptionOrder(AdoptionOrder bdf, User user);
 
-    public List<AdoptionOrder> findAll();
+    public List<AdoptionOrder> findAll(User user);
 
-    public void deleteAdoptionOrder(long idUKey);
+    public void deleteAdoptionOrder(long idUKey, User user);
+
+    public void approveAdoptionOrder(long idUKey, User user);
+
+    public void rejectAdoptionOrder(long idUKey, User user);
 }
