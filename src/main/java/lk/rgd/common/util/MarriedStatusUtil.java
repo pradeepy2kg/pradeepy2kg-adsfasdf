2f6e734f0f924b2138ecf3db21fed2685b295f94
chathuranga <chathuranga@localhost>
2010-08-06 12:20:36 +0530

    still and live birth certificate problems fixed

---
+++ src/main/java/lk/rgd/common/util/MarriedStatusUtil.java
@@ -0,0 +1,44 @@
+package lk.rgd.common.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import lk.rgd.AppConstants;
+
+/**
+ * @author Chathuranga Withana
+ */
+public class MarriedStatusUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(MarriedStatusUtil.class);
+
+    public static String getMarriedStatus(int code, String language) {
+
+        if (AppConstants.SINHALA.equals(language)) {
+            switch (code) {
+                case 0: return "නොදනී";
+                case 1: return "විවාහිකයි";
+                case 2: return "අවිවාහකයි";
+                case 3: return "පසු විවාහයකි";
+            }
+        } else if (AppConstants.TAMIL.equals(language)) {
+            switch (code) {
+                case 0: return "#Unknown#";
+                case 1: return "#Married#";
+                case 2: return "#Unmarried#";
+                case 3: return "#Married Later#";
+            }
+        } else if (AppConstants.ENGLISH.equals(language)) {
+            switch (code) {
+                case 0: return "Unknown"; 
+                case 1: return "Married";
+                case 2: return "Unmarried";
+                case 3: return "Married Later";
+            }
+        } else {
+            logger.error("Invalid language : {}", language);
+            throw new IllegalArgumentException("Invalid language : " + language);
+        }
+        logger.error("Invalid gender code : {}", code);
+        throw new IllegalArgumentException("Invalid gender code : {}");
+    }
+}
--- src/main/java/lk/rgd/crs/api/domain/MarriageInfo.java
+++ src/main/java/lk/rgd/crs/api/domain/MarriageInfo.java
@@ -1,32 +1,45 @@
 package lk.rgd.crs.api.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
 @Embeddable
 public class MarriageInfo implements Serializable {
-    /** Were parents married at birth - 0 - no, 1 - yes, 2 - no but married later  */
+    /**
+     * Were parents married at birth - 0 - unknown, 1 - yes, 2 - no, 3 - no but married later 
+     */
     @Column(nullable = true)
     private Integer parentsMarried;
 
-    /** Place of marriage */
+    /**
+     * Parents married status as a String in the preferred language
+     */
+    @Transient
+    private String parentsMarriedPrint;
+
+    /**
+     * Place of marriage
+     */
     @Column(nullable = true, length = 60)
     private String placeOfMarriage;
 
-    /** Date of marriage */
+    /**
+     * Date of marriage
+     */
     @Column(nullable = true)
     @Temporal(value = TemporalType.DATE)
     private Date dateOfMarriage;
 
-    /** If parents are unmarried - Has the mother signed to include fathers details?  */
+    /**
+     * If parents are unmarried - Has the mother signed to include fathers details?
+     */
     @Column(nullable = true)
     private boolean motherSigned;
 
-    /** If parents are unmarried - Has the father signed to include fathers details? */
+    /**
+     * If parents are unmarried - Has the father signed to include fathers details?
+     */
     @Column(nullable = true)
     private boolean fatherSigned;
 
@@ -36,6 +49,14 @@ public class MarriageInfo implements Serializable {
 
     public void setParentsMarried(Integer parentsMarried) {
         this.parentsMarried = parentsMarried;
+    }
+
+    public String getParentsMarriedPrint() {
+        return parentsMarriedPrint;
+    }
+
+    public void setParentsMarriedPrint(String parentsMarriedPrint) {
+        this.parentsMarriedPrint = parentsMarriedPrint;
     }
 
     public String getPlaceOfMarriage() {
