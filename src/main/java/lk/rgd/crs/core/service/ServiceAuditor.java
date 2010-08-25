fa907f0269903a9684f535c3231d1cef918d7699
asankha <asankha@localhost>
2010-08-25 17:52:00 +0530

    make the method interceptor catch all service method invocations

--- src/main/java/lk/rgd/crs/core/service/ServiceAuditor.java
+++ src/main/java/lk/rgd/crs/core/service/ServiceAuditor.java
@@ -1,14 +1,11 @@
 package lk.rgd.crs.core.service;
 
 import com.thoughtworks.xstream.XStream;
+import lk.rgd.common.RGDRuntimeException;
 import lk.rgd.common.api.dao.EventDAO;
 import lk.rgd.common.api.domain.Event;
 import lk.rgd.common.api.domain.User;
-import lk.rgd.common.core.dao.EventDAOImpl;
 import lk.rgd.crs.api.domain.*;
-import lk.rgd.prs.api.domain.Address;
-import lk.rgd.prs.api.domain.Marriage;
-import lk.rgd.prs.api.domain.Person;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.slf4j.Logger;
@@ -16,57 +13,70 @@ import org.slf4j.LoggerFactory;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.Arrays;
 
 /**
- * Performs auditing of the BirthRegistrationService method invocation
+ * Performs auditing of the Service layer method invocations
  *
  * @author asankha
  */
-public class BirthRegistrationServiceAuditor implements MethodInterceptor {
+public class ServiceAuditor implements MethodInterceptor {
 
-    private static final Logger logger = LoggerFactory.getLogger(BirthRegistrationServiceAuditor.class);
+    private static final Logger logger = LoggerFactory.getLogger(ServiceAuditor.class);
 
     private final XStream xstream = new XStream();
     private final boolean captureDebugBeforeExecution;
     private final EventDAO eventDao;
 
-    public BirthRegistrationServiceAuditor(boolean captureDebugBeforeExecution, EventDAO eventDao) {
+    public ServiceAuditor(boolean captureDebugBeforeExecution, EventDAO eventDao) {
         this.captureDebugBeforeExecution = captureDebugBeforeExecution;
         this.eventDao = eventDao;
+
+        xstream.alias("adoptionOrder", AdoptionOrder.class);
+        xstream.alias("assignment", Assignment.class);
         xstream.alias("birthCertificateSearch", BirthCertificateSearch.class);
         xstream.alias("birthDeclatation", BirthDeclaration.class);
+        xstream.alias("deathRegister", DeathRegister.class);
+        xstream.alias("registrar", Registrar.class);
     }
 
+    /**
+     * Intercepts the service level method call and captures debug information and saves error information
+     * into the Event table within a new and separate transaction
+     * @param methodInvocation not applicable for documentation
+     * @return not applicable for documentation
+     * @throws Throwable not applicable for documentation
+     */
     public Object invoke(MethodInvocation methodInvocation) throws Throwable {
 
         final String methodName = methodInvocation.getMethod().getName();
-        // get hold of the user and bdf if any. Later include death, marriage, person etc too..
+        final String className  = methodInvocation.getClass().getName();
+
+        // get hold of the user and debug info if captureDebugBeforeExecution is enabled
         User user = null;
         Event event = null;
         StringBuilder sb = null;
 
         final Object[] args = methodInvocation.getArguments();
         for (int i=0; i<args.length; i++) {
+            
             if (args[i] instanceof User) {
                 user = (User) args[i];
+                if (!captureDebugBeforeExecution) {
+                    break;  // optimize if possible when not debugging
+                }
 
-            } else if (captureDebugBeforeExecution &&
-                (args[i] instanceof BirthDeclaration || args[i] instanceof BirthCertificateSearch)) {
+            } else if (captureDebugBeforeExecution) {
 
                 if (sb == null) {
-                    sb = new StringBuilder(1024);
+                    sb = new StringBuilder(1024 * 10);
                 }
                 sb.append(xstream.toXML(args[i]));
+                event = new Event();
             }
         }
 
         try {
-            Object result = methodInvocation.proceed();
-            if (captureDebugBeforeExecution) {
-                event = new Event();
-            }
-            return result;
+            return methodInvocation.proceed();
             
         } catch (Exception e) {
 
@@ -76,12 +86,19 @@ public class BirthRegistrationServiceAuditor implements MethodInterceptor {
             e.printStackTrace(pw);
             event.setStackTrace(sw.toString());
 
+            if (e instanceof RGDRuntimeException) {
+                event.setEventCode(((RGDRuntimeException) e).getErrorCode());   
+            }
+
+            event.setEventType(Event.Type.ERROR);
+
             throw e;
 
         } finally {
 
             if (event != null) {
-                event.setEventData(methodName);
+                event.setClassName(className);
+                event.setMethodName(methodName);
 
                 if (user != null) {
                     event.setUser(user);
@@ -91,7 +108,7 @@ public class BirthRegistrationServiceAuditor implements MethodInterceptor {
                 }
 
                 eventDao.addEvent(event);
-                logger.error("Captured event log for issue with ID : {}", event.getIdUKey());
+                logger.debug("Captured event log for issue with ID : {}", event.getIdUKey());
             }
         }
     }
--- src/test/java/lk/rgd/crs/web/action/SampleTest.java
+++ src/test/java/lk/rgd/crs/web/action/SampleTest.java
@@ -5,23 +5,22 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import lk.rgd.common.CustomStrutsTestCase;
-
-import java.util.ArrayList;
-
-import static org.junit.Assert.assertTrue;
-import lk.rgd.common.CustomStrutsTestCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SampleTest extends CustomStrutsTestCase {
 
+    private static final Logger logger = LoggerFactory.getLogger(SampleTest.class);
+    
     public static Test suite() {
         TestSetup setup = new TestSetup(new TestSuite(SampleTest.class)) {
             protected void setUp() throws Exception {
-                System.out.println("===============> show this once for class - setup");
+                logger.debug("=> show this once for class - setup");
                 super.setUp();
             }
 
             protected void tearDown() throws Exception {
-                System.out.println("===============> show this once for class - tear");
+                logger.debug("=> show this once for class - tear");
                 super.tearDown();
             }
         };
@@ -30,30 +29,30 @@ public class SampleTest extends CustomStrutsTestCase {
 
     @Override
     protected void setUp() throws Exception {
-        System.out.println("setup");
+        logger.debug("method setup");
         super.setUp();
     }
 
     @Override
     protected void tearDown() throws Exception {
-        System.out.println("tear");
+        logger.debug("method teardown");
         super.tearDown();
     }
 
     public void testMethod1() {
-        System.out.println("run test method 1");
+        logger.debug("run test method 1");
     }
 
     public void testMethod2() {
-        System.out.println("run test method 2");
+        logger.debug("run test method 2");
     }
 
     public void testMethod3() {
-        System.out.println("run test method 3");
+        logger.debug("run test method 3");
     }
 
     public void testMethod4() {
-        System.out.println("run test method 45");
+        logger.debug("run test method 45");
     }
 
 }
