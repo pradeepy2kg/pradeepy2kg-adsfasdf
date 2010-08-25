b420aa45e13b818653f95a032f3b59e911328370
asankha <asankha@localhost>
2010-08-25 10:46:54 +0530

    add methods to add new BD divisions and intivate/inactivate them

--- src/main/java/lk/rgd/crs/core/dao/BDDivisionDAOImpl.java
+++ src/main/java/lk/rgd/crs/core/dao/BDDivisionDAOImpl.java
@@ -2,9 +2,11 @@ package lk.rgd.crs.core.dao;
 
 import lk.rgd.AppConstants;
 import lk.rgd.ErrorCodes;
+import lk.rgd.Permission;
 import lk.rgd.common.core.dao.BaseDAO;
 import lk.rgd.common.api.domain.User;
 import lk.rgd.common.core.dao.PreloadableDAO;
+import lk.rgd.crs.CRSRuntimeException;
 import lk.rgd.crs.api.dao.BDDivisionDAO;
 import lk.rgd.crs.api.domain.BDDivision;
 import org.springframework.transaction.annotation.Propagation;
@@ -67,6 +69,78 @@ public class BDDivisionDAOImpl extends BaseDAO implements BDDivisionDAO, Preload
     }
 
     /**
+     * Add a new BDDivision and cache
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void add(BDDivision bdDivision, User user) {
+
+        if (isEmptyString(bdDivision.getEnDivisionName()) ||
+            isEmptyString(bdDivision.getEnDivisionName()) ||
+            isEmptyString(bdDivision.getEnDivisionName())) {
+            throw new CRSRuntimeException("One or more names of the BD Division is invalid", ErrorCodes.INVALID_DATA);
+        }
+
+        if (user.isAuthorized(Permission.ADD_EDIT_DIVISIONS)) {
+            try {
+                em.persist(bdDivision);
+                updateCache(bdDivision);
+                logger.info("New BD Division added : {} by : {}", bdDivision.getEnDivisionName(), user.getUserId());
+            } catch (Exception e) {
+                logger.error("Attempt to add BD Division : " + bdDivision.getEnDivisionName() + " failed", e);
+            }
+        } else {
+            logger.error("User : " + user.getUserId() +
+                " was not allowed to add a new BD Division : " + bdDivision.getEnDivisionName());
+        }
+    }
+
+    /**
+     * Inactivate a BDDivision and update cache
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void inactivate(BDDivision bdDivision, User user) {
+        if (user.isAuthorized(Permission.ADD_EDIT_DIVISIONS)) {
+            try {
+                BDDivision existing = em.find(BDDivision.class, bdDivision.getBdDivisionUKey());
+                if (existing != null) {
+                    existing.setActive(false);
+                    em.merge(existing);
+                    updateCache(existing);
+                }
+                logger.info("BD Division : {} inactivated by : {}", bdDivision.getEnDivisionName(), user.getUserId());
+            } catch (Exception e) {
+                logger.error("Attempt to inactivate BD Division : " + bdDivision.getEnDivisionName() + " failed", e);
+            }
+        } else {
+            logger.error("User : " + user.getUserId() +
+                " was not allowed to inactivate BD Division : " + bdDivision.getEnDivisionName());
+        }
+    }
+
+    /**
+     * Activate a BDDivision and update cache
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void activate(BDDivision bdDivision, User user) {
+        if (user.isAuthorized(Permission.ADD_EDIT_DIVISIONS)) {
+            try {
+                BDDivision existing = em.find(BDDivision.class, bdDivision.getBdDivisionUKey());
+                if (existing != null) {
+                    existing.setActive(true);
+                    em.merge(existing);
+                    updateCache(existing);
+                }
+                logger.info("BD Division : {} activated by : {}", bdDivision.getEnDivisionName(), user.getUserId());
+            } catch (Exception e) {
+                logger.error("Attempt to activate BD Division : " + bdDivision.getEnDivisionName() + " failed", e);
+            }
+        } else {
+            logger.error("User : " + user.getUserId() +
+                " was not allowed to activate BD Division : " + bdDivision.getEnDivisionName());
+        }
+    }
+
+    /**
      * Loads all values from the database table into a cache
      */
     @Transactional(propagation = Propagation.NEVER, readOnly = true)
@@ -76,35 +150,38 @@ public class BDDivisionDAOImpl extends BaseDAO implements BDDivisionDAO, Preload
         List<BDDivision> results = query.getResultList();
 
         for (BDDivision r : results) {
-            //final int districtUKey   = r.getBirthDistrictId().getDistrictUKey();
-            final int dsDivisionUKey = r.getDsDivision().getDsDivisionUKey();
-            final int bdDivisionId   = r.getDivisionId();
-            final int bdDivisionUKey = r.getBdDivisionUKey();
-
-            bdDivisionsByPK.put(bdDivisionUKey, r);
-
-            Map<Integer, String> districtMap = siDivisions.get(dsDivisionUKey);
-            if (districtMap == null) {
-                districtMap = new HashMap<Integer, String>();
-                siDivisions.put(dsDivisionUKey, districtMap);
-            }
-            districtMap.put(bdDivisionUKey, bdDivisionId + ": " + r.getSiDivisionName());
-
-            districtMap = enDivisions.get(dsDivisionUKey);
-            if (districtMap == null) {
-                districtMap = new HashMap<Integer, String>();
-                enDivisions.put(dsDivisionUKey, districtMap);
-            }
-            districtMap.put(bdDivisionUKey, bdDivisionId + SPACER + r.getEnDivisionName());
-
-            districtMap = taDivisions.get(dsDivisionUKey);
-            if (districtMap == null) {
-                districtMap = new HashMap<Integer, String>();
-                taDivisions.put(dsDivisionUKey, districtMap);
-            }
-            districtMap.put(bdDivisionUKey, bdDivisionId + SPACER + r.getTaDivisionName());
+            updateCache(r);
         }
 
         logger.debug("Loaded : {} birth and death registration divisions from the database", results.size());
+    }
+
+    private void updateCache(BDDivision r) {
+        final int dsDivisionUKey = r.getDsDivision().getDsDivisionUKey();
+        final int bdDivisionId   = r.getDivisionId();
+        final int bdDivisionUKey = r.getBdDivisionUKey();
+
+        bdDivisionsByPK.put(bdDivisionUKey, r);
+
+        Map<Integer, String> districtMap = siDivisions.get(dsDivisionUKey);
+        if (districtMap == null) {
+            districtMap = new HashMap<Integer, String>();
+            siDivisions.put(dsDivisionUKey, districtMap);
+        }
+        districtMap.put(bdDivisionUKey, bdDivisionId + ": " + r.getSiDivisionName());
+
+        districtMap = enDivisions.get(dsDivisionUKey);
+        if (districtMap == null) {
+            districtMap = new HashMap<Integer, String>();
+            enDivisions.put(dsDivisionUKey, districtMap);
+        }
+        districtMap.put(bdDivisionUKey, bdDivisionId + SPACER + r.getEnDivisionName());
+
+        districtMap = taDivisions.get(dsDivisionUKey);
+        if (districtMap == null) {
+            districtMap = new HashMap<Integer, String>();
+            taDivisions.put(dsDivisionUKey, districtMap);
+        }
+        districtMap.put(bdDivisionUKey, bdDivisionId + SPACER + r.getTaDivisionName());
     }
 }
