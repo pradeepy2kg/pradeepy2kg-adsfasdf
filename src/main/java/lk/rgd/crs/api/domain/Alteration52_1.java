527be5d79f2d753518fc77c9938b08a321d1b625
ashokae <ashokae@localhost>
2010-09-06 16:38:12 +0530

    child information included. removed register object

--- src/main/java/lk/rgd/crs/api/domain/Alteration52_1.java
+++ src/main/java/lk/rgd/crs/api/domain/Alteration52_1.java
@@ -1,8 +1,7 @@
 package lk.rgd.crs.api.domain;
 
-import javax.persistence.Embedded;
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  * @author Ashoka Ekanayaka
@@ -11,8 +10,38 @@ import javax.persistence.Embeddable;
  */
 @Embeddable
 public class Alteration52_1 {
-    @Embedded
-    private BirthRegisterInfo register;
+    /**
+     * The date of the birth
+     */
+    @Column(nullable = false)
+    @Temporal(value = TemporalType.DATE)
+    private Date dateOfBirth;
+
+    /**
+     * The place of birth - usually the village or hospital name
+     */
+    @Column(nullable = true, length = 255)
+    private String placeOfBirth;
+
+    /**
+     * The place of birth in English - usually the village or hospital name
+     */
+    @Column(nullable = true, length = 255)
+    private String placeOfBirthEnglish;
+
+
+    /**
+     * The Birth/Death registration division where the birth is registered (Includes District)
+     */
+    @ManyToOne
+    @JoinColumn(name = "bdDivisionUKey", nullable = false)
+    private BDDivision birthDivision;
+
+    /**
+     * Gender 0 - male, 1 - female, 2 - unknown
+     */
+    @Column(nullable = false)
+    private int childGender;
 
     @Embedded
     private MotherInfo mother;
@@ -22,14 +51,6 @@ public class Alteration52_1 {
 
     @Column(nullable = true, length = 1000)
     private String natureOfError;
-
-    public BirthRegisterInfo getRegister() {
-        return register;
-    }
-
-    public void setRegister(BirthRegisterInfo register) {
-        this.register = register;
-    }
 
     public MotherInfo getMother() {
         return mother;
@@ -53,5 +74,45 @@ public class Alteration52_1 {
 
     public void setNatureOfError(String natureOfError) {
         this.natureOfError = natureOfError;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+        this.placeOfBirth = placeOfBirth;
+    }
+
+    public String getPlaceOfBirthEnglish() {
+        return placeOfBirthEnglish;
+    }
+
+    public void setPlaceOfBirthEnglish(String placeOfBirthEnglish) {
+        this.placeOfBirthEnglish = placeOfBirthEnglish;
+    }
+
+    public BDDivision getBirthDivision() {
+        return birthDivision;
+    }
+
+    public void setBirthDivision(BDDivision birthDivision) {
+        this.birthDivision = birthDivision;
+    }
+
+    public int getChildGender() {
+        return childGender;
+    }
+
+    public void setChildGender(int childGender) {
+        this.childGender = childGender;
     }
 }
