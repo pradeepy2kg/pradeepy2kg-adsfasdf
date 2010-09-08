fee10fbde2337fc53cd7d87444c7294f05085c61
htpunchihewa <htpunchihewa@localhost>
2010-09-08 13:50:43 +0530

    uncommited changes

--- src/main/java/lk/rgd/crs/web/action/births/BirthRegisterAction.java
+++ src/main/java/lk/rgd/crs/web/action/births/BirthRegisterAction.java
@@ -866,6 +866,7 @@ public class BirthRegisterAction extends ActionSupport implements SessionAware {
             case 2:
                 bdf = service.getByPINorNIC(nicOrPin, user);
                 break;
+            
             case 3:
                 bdf = service.getActiveRecordByBDDivisionAndSerialNo(bdDivisionDAO.getBDDivisionByPK(birthDivisionId),
                         serialNo, user);
