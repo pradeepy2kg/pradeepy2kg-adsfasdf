7cb6b954696793c8337dad00c1db8e4c3c7446e6
asankha <asankha@asankha>
2010-11-02 14:31:50 +0530

    add a method to get status as a character for solr, minor reformatting and spelling checks

--- src/main/java/lk/rgd/common/util/LifeStatusUtil.java
+++ src/main/java/lk/rgd/common/util/LifeStatusUtil.java
@@ -15,60 +15,76 @@ import lk.rgd.AppConstants;
 public class LifeStatusUtil {
     private static final Logger logger = LoggerFactory.getLogger(LifeStatusUtil.class);
 
-        /**
-         * Return life status string in selected language
-         *
-         * @param state  the life status enumeration
-         * @param language selected language
-         * @return the life status string for selected language and enumeration
-         * @see lk.rgd.prs.api.domain.Person.LifeStatus
-         */
-        public static String getLifeStatus(Person.LifeStatus state, String language) {
-
-            if (AppConstants.SINHALA.equals(language)) {
-                switch (state) {
-                    case UNKNOWN:
-                        return "නොදනී ";
-                    case ALIVE:
-                        return "ජීවතුන් අතර ඇත ";
-                    case DEAD:
-                        return "ජීවතුන් අතර නැත  ";
-                    case MISSING:
-                        return "අතුරුදන් ";
-                    case NON_RESIDENT:
-                        return "පදිංචිව නැත ";
-                }
-            } else if (AppConstants.TAMIL.equals(language)) {
-                switch (state) {
-                    case UNKNOWN:
-                        return "Unknown";
-                    case ALIVE:
-                        return "Alive";
-                    case DEAD:
-                        return "Dead";
-                    case MISSING:
-                        return "Missing";
-                    case NON_RESIDENT:
-                        return "Non resident";
-                }
-            } else if (AppConstants.ENGLISH.equals(language)) {
-                switch (state) {
-                    case UNKNOWN:
-                        return "Unknown";
-                    case ALIVE:
-                        return "Alive";
-                    case DEAD:
-                        return "Dead";
-                    case MISSING:
-                        return "Missing";
-                    case NON_RESIDENT:
-                        return "Non resident";
-                }
-            } else {
-                logger.error("Invalid language : {}", language);
-                throw new IllegalArgumentException("Invalid language : " + language);
-            }
-            logger.error("Invalid life status : {}", state);
-            throw new IllegalArgumentException("Invalid life status : " + state);
+    public static String getStatusAsString(Person.LifeStatus s) {
+        switch (s) {
+            case UNKNOWN:
+                return "U";
+            case ALIVE:
+                return "A";
+            case DEAD:
+                return "D";
+            case MISSING:
+                return "M";
+            case NON_RESIDENT:
+                return "N";
         }
+        throw new IllegalArgumentException("Illegal civil status : " + s);
+    }
+
+    /**
+     * Return life status string in selected language
+     *
+     * @param state    the life status enumeration
+     * @param language selected language
+     * @return the life status string for selected language and enumeration
+     * @see lk.rgd.prs.api.domain.Person.LifeStatus
+     */
+    public static String getLifeStatus(Person.LifeStatus state, String language) {
+
+        if (AppConstants.SINHALA.equals(language)) {
+            switch (state) {
+                case UNKNOWN:
+                    return "නොදනී ";
+                case ALIVE:
+                    return "ජීවතුන් අතර ඇත ";
+                case DEAD:
+                    return "ජීවතුන් අතර නැත  ";
+                case MISSING:
+                    return "අතුරුදන් ";
+                case NON_RESIDENT:
+                    return "පදිංචිව නැත ";
+            }
+        } else if (AppConstants.TAMIL.equals(language)) {
+            switch (state) {
+                case UNKNOWN:
+                    return "Unknown";
+                case ALIVE:
+                    return "Alive";
+                case DEAD:
+                    return "Dead";
+                case MISSING:
+                    return "Missing";
+                case NON_RESIDENT:
+                    return "Non resident";
+            }
+        } else if (AppConstants.ENGLISH.equals(language)) {
+            switch (state) {
+                case UNKNOWN:
+                    return "Unknown";
+                case ALIVE:
+                    return "Alive";
+                case DEAD:
+                    return "Dead";
+                case MISSING:
+                    return "Missing";
+                case NON_RESIDENT:
+                    return "Non resident";
+            }
+        } else {
+            logger.error("Invalid language : {}", language);
+            throw new IllegalArgumentException("Invalid language : " + language);
+        }
+        logger.error("Invalid life status : {}", state);
+        throw new IllegalArgumentException("Invalid life status : " + state);
+    }
 }
