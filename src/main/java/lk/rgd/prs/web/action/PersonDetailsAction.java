8d54f94978e6f90d6fe5d2b6a36e13b93217bd6b
htpunchihewa <htpunchihewa@localhost>
2010-11-03 10:50:32 +0530

    implement a max number of incorrect login attempts

---
+++ src/main/java/lk/rgd/prs/web/action/PersonDetailsAction.java
@@ -0,0 +1,259 @@
+package lk.rgd.prs.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.interceptor.SessionAware;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import lk.rgd.prs.api.service.PopulationRegistry;
+import lk.rgd.prs.api.domain.Person;
+import lk.rgd.prs.api.domain.Address;
+import lk.rgd.prs.api.domain.PersonCitizenship;
+import lk.rgd.prs.api.domain.Marriage;
+import lk.rgd.common.api.dao.RaceDAO;
+import lk.rgd.common.api.dao.CountryDAO;
+import lk.rgd.common.api.domain.User;
+import lk.rgd.common.util.GenderUtil;
+import lk.rgd.common.util.CivilStatusUtil;
+import lk.rgd.common.util.LifeStatusUtil;
+import lk.rgd.crs.web.WebConstants;
+import lk.rgd.crs.api.domain.MarriageInfo;
+import lk.rgd.AppConstants;
+
+import java.util.*;
+
+public class PersonDetailsAction extends ActionSupport implements SessionAware {
+
+    private static final Logger logger = LoggerFactory.getLogger(PersonDetailsAction.class);
+
+    // services and DAOs
+    private final PopulationRegistry service;
+    private final RaceDAO raceDAO;
+    private final CountryDAO countryDAO;
+
+    private Map session;
+    private User user;
+
+    private String gender;
+    private String genderEn;
+    private String lifeStatus;
+    private String race;
+    private String raceEn;
+    private String civilStatus;
+    private MarriageInfo marriage;
+    private Address permanentAddress;
+    private Set<Address> address;
+    private Set<PersonCitizenship> personCitizenship;
+    private Set<Marriage> marrage;
+    private int numberOfCountries;
+
+    private Person person;
+    private long personId;
+    private List<Person> children;
+    private List<Person> siblings;
+
+    public PersonDetailsAction(PopulationRegistry service, RaceDAO raceDAO, CountryDAO countryDAO) {
+        this.service = service;
+        this.raceDAO = raceDAO;
+        this.countryDAO = countryDAO;
+    }
+
+    public String personDetails() {
+        logger.debug("getting extended details of an existing person in PRS");
+        person = service.getByUKey(personId, user);
+        gender = GenderUtil.getGender(person.getGender(), user.getPrefLanguage());
+        if (person.getRace() != null) {
+            race = raceDAO.getNameByPK(person.getRace().getRaceId(), user.getPrefLanguage());
+        }
+        if (person.getCivilStatus() != null) {
+            civilStatus = CivilStatusUtil.getCivilStatus(person.getCivilStatus(), user.getPrefLanguage());
+        }
+        if (person.getLifeStatus() != null) {
+            lifeStatus = LifeStatusUtil.getLifeStatus(person.getLifeStatus(), user.getPrefLanguage());
+        }
+        if (person.getAddresses() != null) {
+            address=person.getAddresses();
+        }
+        if(person.getMarriages() != null){
+            marrage = person.getMarriages();
+        }
+        
+        children = service.findAllChildren(person, user);
+        logger.debug("number of children for {} is {}", person.getFullNameInOfficialLanguage(), children.size());
+
+        siblings = service.findAllSiblings(person, user);
+        logger.debug("number of siblings for {} is {}", person.getFullNameInOfficialLanguage(), siblings.size());
+        return SUCCESS;
+    }
+
+    /**
+     * This method is used to load PRS certificate
+     */
+    public String initPRSCertificate() {
+        person = service.getLoadedObjectByUKey(personId, user);
+        gender = GenderUtil.getGender(person.getGender(), person.getPreferredLanguage());
+        genderEn = GenderUtil.getGender(person.getGender(), AppConstants.ENGLISH);
+        if (person.getRace() != null) {
+            race = raceDAO.getNameByPK(person.getRace().getRaceId(), person.getPreferredLanguage());
+            raceEn = raceDAO.getNameByPK(person.getRace().getRaceId(), AppConstants.ENGLISH);
+        }
+        if (person.getAddresses() != null) {
+            for (Address address : person.getAddresses()) {
+                if (address.isPermanent()) {
+                    permanentAddress = address;
+                    break;
+                }
+            }
+        }
+        if (person.getCountries() != null) {
+            personCitizenship = person.getCountries();
+            numberOfCountries = personCitizenship.size();
+        }
+        return SUCCESS;
+    }
+
+    /**
+     * This method is used to load existing person registration form
+     */
+    public String personRegistrationInit() {
+        logger.debug("getting extended details of an existing person in PRS");
+        return SUCCESS;
+    }
+
+    public Map getSession() {
+        return session;
+    }
+
+    public void setSession(Map session) {
+        this.session = session;
+        user = (User) session.get(WebConstants.SESSION_USER_BEAN);
+        logger.debug("setting User: {}", user.getUserName());
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(long personId) {
+        this.personId = personId;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Person> children) {
+        this.children = children;
+    }
+
+    public List getSiblings() {
+        return siblings;
+    }
+
+    public void setSiblings(List siblings) {
+        this.siblings = siblings;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLifeStatus() {
+        return lifeStatus;
+    }
+
+    public void setLifeStatus(String lifeStatus) {
+        this.lifeStatus = lifeStatus;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getCivilStatus() {
+        return civilStatus;
+    }
+
+    public void setCivilStatus(String civilStatus) {
+        this.civilStatus = civilStatus;
+    }
+
+    public MarriageInfo getMarriage() {
+        return marriage;
+    }
+
+    public void setMarriage(MarriageInfo marriage) {
+        this.marriage = marriage;
+    }
+
+    public String getGenderEn() {
+        return genderEn;
+    }
+
+    public void setGenderEn(String genderEn) {
+        this.genderEn = genderEn;
+    }
+
+    public String getRaceEn() {
+        return raceEn;
+    }
+
+    public void setRaceEn(String raceEn) {
+        this.raceEn = raceEn;
+    }
+
+    public Address getPermanentAddress() {
+        return permanentAddress;
+    }
+
+    public void setPermanentAddress(Address permanentAddress) {
+        this.permanentAddress = permanentAddress;
+    }
+
+    public Set<PersonCitizenship> getPersonCitizenship() {
+        return personCitizenship;
+    }
+
+    public void setPersonCitizenship(Set<PersonCitizenship> personCitizenship) {
+        this.personCitizenship = personCitizenship;
+    }
+
+    public int getNumberOfCountries() {
+        return numberOfCountries;
+    }
+
+    public void setNumberOfCountries(int numberOfCountries) {
+        this.numberOfCountries = numberOfCountries;
+    }
+
+    public Set<Address> getAddress() {
+        return address;
+    }
+
+    public void setAddress(Set<Address> address) {
+        this.address = address;
+    }
+
+    public Set<Marriage> getMarrage() {
+        return marrage;
+    }
+
+    public void setMarrage(Set<Marriage> marrage) {
+        this.marrage = marrage;
+    }
+}
---
+++ src/main/java/lk/rgd/prs/web/action/PopulationRegisterAction.java
@@ -0,0 +1,276 @@
+package lk.rgd.prs.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import lk.rgd.common.api.dao.CountryDAO;
+import lk.rgd.common.api.dao.RaceDAO;
+import lk.rgd.common.api.domain.Country;
+import lk.rgd.common.api.domain.User;
+import lk.rgd.common.util.DateTimeUtils;
+import lk.rgd.common.util.NameFormatUtil;
+import lk.rgd.common.util.WebUtils;
+import lk.rgd.crs.web.WebConstants;
+import lk.rgd.prs.api.domain.Person;
+import lk.rgd.prs.api.domain.PersonCitizenship;
+import lk.rgd.prs.api.service.PopulationRegistry;
+import org.apache.struts2.interceptor.SessionAware;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+/**
+ * @author Chathuranga Withana
+ */
+public class PopulationRegisterAction extends ActionSupport implements SessionAware {
+
+    private static final Logger logger = LoggerFactory.getLogger(PopulationRegisterAction.class);
+    // This delimiter used to delimit countryId and passportNo from citizenship string
+    private static final String delimiter = ":,";
+
+    // services and DAOs
+    private final PopulationRegistry service;
+    private final RaceDAO raceDAO;
+    private final CountryDAO countryDAO;
+
+    private Map<Integer, String> raceList;
+    private Map<Integer, String> countryList;
+    private List<Person> personList;
+    private List<PersonCitizenship> citizenshipList;
+
+    private Map session;
+    private User user;
+
+    private Person person;
+
+    private long personUKey;
+    private int personCountryId;
+    private int personRaceId;
+    private String personPassportNo;
+    private String permanentAddress;
+    private String currentAddress;
+    private String citizenship;
+
+    public PopulationRegisterAction(PopulationRegistry service, RaceDAO raceDAO, CountryDAO countryDAO) {
+        this.service = service;
+        this.raceDAO = raceDAO;
+        this.countryDAO = countryDAO;
+    }
+
+    /**
+     * This method used to register persons to the PRS
+     */
+    public String personRegistration() {
+        logger.debug("Registration of exiting person to PRS");
+        // TODO implement validation 
+        validateExistingPersonRegistration();
+
+//        final List<PersonCitizenship> citizenshipList = new ArrayList<PersonCitizenship>();
+        citizenshipList = new ArrayList<PersonCitizenship>();
+        getCitizenshipList(citizenshipList);
+        if (personCountryId != 0 && personPassportNo != null) {
+            citizenshipList.add(createPersonCitizenship(countryDAO.getCountry(personCountryId), personPassportNo));
+        }
+        personList = service.addExistingPerson(person, permanentAddress, currentAddress, user, citizenshipList);
+
+        if (personList.isEmpty()) {
+            final long pin = person.getPin();
+            // personUKey used to redirect to PRS certificate page
+            personUKey = person.getPersonUKey();
+            addActionMessage(getText("person_reg_success.message") + pin);
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("Person with name : " + NameFormatUtil.getDisplayName(person.getFullNameInEnglishLanguage(), 30)
+                    + " and dateOfBirth : " + DateTimeUtils.getISO8601FormattedString(person.getDateOfBirth())
+                    + " added to the PRS with PersonUKey : " + person.getPersonUKey() + " and generated PIN : " + pin);
+            }
+            return SUCCESS;
+        } else {
+            populate();
+            return "form";
+        }
+    }
+
+    /**
+     * This method edits existing persons in the PRS
+     */
+    public String editPersonDetails() {
+        logger.debug("Edit Person Details with PersonUKey : {}", personUKey);
+        populate();
+        person = service.getByUKey(personUKey, user);
+        return SUCCESS;
+    }
+
+    /**
+     * Returns citizenship list after processing citizenship String passed by the Existing Person Registration Form
+     */
+    private void getCitizenshipList(List<PersonCitizenship> list) {
+        logger.debug("Received citizenship string : {}", citizenship);
+//        List<PersonCitizenship> list = new ArrayList<PersonCitizenship>();
+        if (citizenship != null && citizenship.trim().length() > 0) {
+
+            int countryId;
+            String passportNo;
+            StringTokenizer tokenizer = new StringTokenizer(citizenship, delimiter);
+            while (tokenizer.hasMoreTokens()) {
+                countryId = Integer.parseInt(tokenizer.nextToken());
+                passportNo = tokenizer.nextToken();
+                list.add(createPersonCitizenship(countryDAO.getCountry(countryId), passportNo));
+            }
+
+//            return list;
+        } else {
+//            return null;
+        }
+    }
+
+    /**
+     * Return PersonCitizenship object when country and passport number passed
+     */
+    private PersonCitizenship createPersonCitizenship(Country country, String passport) {
+        PersonCitizenship pc = new PersonCitizenship();
+        pc.setCountry(country);
+        pc.setPassportNo(passport);
+        return pc;
+    }
+
+    /**
+     * Validations for existing person registration form
+     */
+    private void validateExistingPersonRegistration() {
+        // TODO validate inputs
+        /*if (person.getDateOfRegistration() == null || person.getDateOfBirth() == null || person.getPlaceOfBirth() == null
+            || person.getFullNameInOfficialLanguage() == null || person.getFullNameInEnglishLanguage() == null) {
+
+        }*/
+    }
+
+    /**
+     * This method is used to load existing person registration form
+     */
+    public String personRegistrationInit() {
+        logger.debug("Registration of existing person to PRS page loaded");
+        populate();
+        return SUCCESS;
+    }
+
+    /**
+     * Populate master data (race list, country list etc.) to the UI
+     */
+    private void populate() {
+        logger.debug("Populating initializing data");
+        String language = ((Locale) session.get(WebConstants.SESSION_USER_LANG)).getLanguage();
+
+        raceList = raceDAO.getRaces(language);
+        countryList = countryDAO.getCountries(language);
+        logger.debug("Race list and Country list populated with size : {} and {}", raceList.size(), countryList.size());
+    }
+
+    public Map getSession() {
+        return session;
+    }
+
+    public void setSession(Map session) {
+        this.session = session;
+        user = (User) session.get(WebConstants.SESSION_USER_BEAN);
+        logger.debug("setting User: {}", user.getUserName());
+    }
+
+    public Map<Integer, String> getRaceList() {
+        return raceList;
+    }
+
+    public void setRaceList(Map<Integer, String> raceList) {
+        this.raceList = raceList;
+    }
+
+    public Map<Integer, String> getCountryList() {
+        return countryList;
+    }
+
+    public void setCountryList(Map<Integer, String> countryList) {
+        this.countryList = countryList;
+    }
+
+    public List<Person> getPersonList() {
+        return personList;
+    }
+
+    public void setPersonList(List<Person> personList) {
+        this.personList = personList;
+    }
+
+    public List<PersonCitizenship> getCitizenshipList() {
+        return citizenshipList;
+    }
+
+    public void setCitizenshipList(List<PersonCitizenship> citizenshipList) {
+        this.citizenshipList = citizenshipList;
+    }
+
+    public long getPersonUKey() {
+        return personUKey;
+    }
+
+    public void setPersonUKey(long personUKey) {
+        this.personUKey = personUKey;
+    }
+
+    public int getPersonCountryId() {
+        return personCountryId;
+    }
+
+    public void setPersonCountryId(int personCountryId) {
+        this.personCountryId = personCountryId;
+    }
+
+    public int getPersonRaceId() {
+        return personRaceId;
+    }
+
+    public void setPersonRaceId(int personRaceId) {
+        this.personRaceId = personRaceId;
+        person.setRace(raceDAO.getRace(personRaceId));
+        logger.debug("setting person Race : {}", person.getRace().getEnRaceName());
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public String getPersonPassportNo() {
+        return personPassportNo;
+    }
+
+    public void setPersonPassportNo(String personPassportNo) {
+        this.personPassportNo = personPassportNo;
+    }
+
+    public String getPermanentAddress() {
+        return permanentAddress;
+    }
+
+    public void setPermanentAddress(String permanentAddress) {
+        this.permanentAddress = WebUtils.filterBlanksAndToUpper(permanentAddress);
+    }
+
+    public String getCurrentAddress() {
+        return currentAddress;
+    }
+
+    public void setCurrentAddress(String currentAddress) {
+        this.currentAddress = WebUtils.filterBlanksAndToUpper(currentAddress);
+    }
+
+    public String getCitizenship() {
+        return citizenship;
+    }
+
+    public void setCitizenship(String citizenship) {
+        this.citizenship = citizenship;
+        logger.debug("setting citizenship list : {}", citizenship);
+    }
+}
