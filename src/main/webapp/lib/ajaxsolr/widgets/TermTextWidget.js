7ad163a52b89404e77f1d8ecd19ab39ad3c9b587
kalpanie <kalpanie@localhost>
2010-11-16 15:18:51 +0530

    proper wordings in registrar details

---
+++ src/main/webapp/lib/ajaxsolr/widgets/TermTextWidget.js
@@ -0,0 +1,28 @@
+(function ($) {
+
+AjaxSolr.TermTextWidget = AjaxSolr.AbstractFacetWidget.extend({
+
+  afterRequest: function () {
+    $(this.target).find('input').val('');
+
+    var self = this;
+    $(this.target).find('input').bind('keydown', function(e) {
+      if (e.which == 13) {
+        var value = '"' + $(this).val() + '"~40';
+        if (value && self.add(value)) {
+          self.manager.doRequest(0);
+        }
+      }
+    });
+  },
+
+  /**
+   * do not enclose again with double quotes
+   */
+  fq: function (value, exclude) {
+    return (exclude ? '-' : '') + this.field + ':' + value;
+  }
+  
+});
+
+})(jQuery);
