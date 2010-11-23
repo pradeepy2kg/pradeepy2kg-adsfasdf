0951f008a07570b15c09d04886acfb1824f67f1f
ashokas <ashokas@localhost>
2010-11-23 10:17:50 +0530

    Witness Class

---
+++ src/main/java/lk/rgd/crs/api/domain/Witness.java
@@ -0,0 +1,68 @@
+package lk.rgd.crs.api.domain;
+
+import javax.persistence.*;
+
+/**
+ * @author Ashoka Ekanayaka
+ * Entity to hold witness information in Marriages.
+ */
+@Entity
+@Table(name = "WITNESS", schema = "CRS")
+
+public class Witness {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idukey;
+
+    @Column(length = 10)
+    private String identificationNumber;
+
+    @Column(length = 600)
+    private String fullName;
+
+    @Column(length = 255)
+    private String rankOrProfession;
+
+    @Column(length = 255)
+    private String address;
+
+    public long geIdUKey() {
+        return idukey;
+    }
+
+    public void setIdukey(long idukey) {
+        this.idukey = idukey;
+    }
+
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRankOrProfession() {
+        return rankOrProfession;
+    }
+
+    public void setRankOrProfession(String rankOrProfession) {
+        this.rankOrProfession = rankOrProfession;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
