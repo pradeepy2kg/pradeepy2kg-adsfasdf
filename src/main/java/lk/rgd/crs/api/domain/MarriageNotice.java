3f8fe76a4d9bcfcee03f6469eb27f8de06c2aced
chathuranga <chathuranga@localhost>
2010-11-30 16:22:49 +0530

    make MarriageNotice to a immutable object

--- src/main/java/lk/rgd/crs/api/domain/MarriageNotice.java
+++ src/main/java/lk/rgd/crs/api/domain/MarriageNotice.java
@@ -1,10 +1,59 @@
 package lk.rgd.crs.api.domain;
 
+import java.util.Date;
+
 /**
- * An instance used in listing marriage notices 
+ * An instance used to list marriage notices
  *
  * @author Chathuranga Withana
  */
-public class MarriageNotice {
-    
+public final class MarriageNotice {
+
+    private final long idUKey;
+    private final MarriageRegister.TypeOfMarriage typeOfMarriage;
+    private final MarriageRegister.PlaceOfMarriage placeOfMarriage;
+    private final String serialOfNotice;
+    private final Date dateOfNotice;
+    private final String partyNameInOfficialLang;
+    private final String partyPIN;
+
+    public MarriageNotice(long idUKey, MarriageRegister.TypeOfMarriage typeOfMarriage,
+        MarriageRegister.PlaceOfMarriage placeOfMarriage, String serialOfNotice, Date dateOfNotice,
+        String partyNameInOfficialLang, String partyPIN) {
+        this.idUKey = idUKey;
+        this.typeOfMarriage = typeOfMarriage;
+        this.placeOfMarriage = placeOfMarriage;
+        this.serialOfNotice = serialOfNotice;
+        this.dateOfNotice = dateOfNotice;
+        this.partyNameInOfficialLang = partyNameInOfficialLang;
+        this.partyPIN = partyPIN;
+    }
+
+    public long getIdUKey() {
+        return idUKey;
+    }
+
+    public MarriageRegister.TypeOfMarriage getTypeOfMarriage() {
+        return typeOfMarriage;
+    }
+
+    public MarriageRegister.PlaceOfMarriage getPlaceOfMarriage() {
+        return placeOfMarriage;
+    }
+
+    public String getSerialOfNotice() {
+        return serialOfNotice;
+    }
+
+    public Date getDateOfNotice() {
+        return new Date(dateOfNotice.getTime());
+    }
+
+    public String getPartyNameInOfficialLang() {
+        return partyNameInOfficialLang;
+    }
+
+    public String getPartyPIN() {
+        return partyPIN;
+    }
 }
