0809f916975401f36a9bb40610c224cf77ab3f0c
chathuranga <chathuranga@localhost>
2010-12-30 09:57:27 +0530

    queries in Person updated

--- src/main/java/lk/rgd/prs/api/domain/Person.java
+++ src/main/java/lk/rgd/prs/api/domain/Person.java
@@ -21,8 +21,8 @@ import java.util.Set;
     @NamedQuery(name = "filter.by.pin", query = "SELECT p FROM Person p WHERE p.pin = :pin AND p.lifeCycleInfo.activeRecord IS TRUE"),
     @NamedQuery(name = "filter.by.temporaryPIN", query = "SELECT p FROM Person p WHERE p.temporaryPin = :temporaryPin AND p.lifeCycleInfo.activeRecord IS TRUE"),
     @NamedQuery(name = "filter.by.nic", query = "SELECT p FROM Person p WHERE p.nic = :nic AND p.lifeCycleInfo.activeRecord IS TRUE"),
-    @NamedQuery(name = "findAllChildren", query = "SELECT p FROM Person p WHERE p.lifeCycleInfo.activeRecord IS TRUE AND p.mother = :person OR p.father = :person"),
-    @NamedQuery(name = "findAllSiblings", query = "SELECT p FROM Person p WHERE p.lifeCycleInfo.activeRecord IS TRUE AND (p.mother = :mother OR p.father = :father)"),
+    @NamedQuery(name = "findAllChildren", query = "SELECT p FROM Person p WHERE (p.mother = :person OR p.father = :person) AND p.lifeCycleInfo.activeRecord IS TRUE"),
+    @NamedQuery(name = "findAllSiblings", query = "SELECT p FROM Person p WHERE (p.mother = :mother OR p.father = :father) AND p.lifeCycleInfo.activeRecord IS TRUE"),
     @NamedQuery(name = "get.by.location", query = "SELECT p FROM Person p WHERE p.submittedLocation = :location AND p.status <= 3 AND p.lifeCycleInfo.activeRecord IS TRUE " +
         "ORDER BY p.lifeCycleInfo.lastUpdatedTimestamp DESC"),
     @NamedQuery(name = "get.by.location.and.pin", query = "SELECT p FROM Person p WHERE p.submittedLocation = :location " +
