63a07f3f9db4e6316cbd434260d4fd8d94ac885e
asankha <asankha@localhost6.localdomain6>
2011-01-03 18:06:17 +0530

    fix bugs and checkin unit test for content repo

--- src/test/java/lk/rgd/crs/core/service/ContentRepositoryImplTest.java
+++ src/test/java/lk/rgd/crs/core/service/ContentRepositoryImplTest.java
@@ -1,5 +1,6 @@
 package lk.rgd.crs.core.service;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 import java.io.File;
@@ -12,6 +13,10 @@ public class ContentRepositoryImplTest extends TestCase {
 
     public void testContentRepository() throws Exception {
 
+        try {
+            Runtime.getRuntime().exec("rm -rf /tmp/cr");
+        } catch (Exception ignore) {}
+
         ContentRepository cr = new ContentRepositoryImpl("/tmp/cr");
 
         File f = File.createTempFile("temp", ".data");
@@ -19,7 +24,15 @@ public class ContentRepositoryImplTest extends TestCase {
         fos.write("Hello World".getBytes());
         fos.close();
 
-        String path = cr.storeFile(1, 1, f);
-        System.out.println(path);
+        int division = 1;
+        for (int i=1; i<=260; i++) {
+            String path = cr.storeFile(division, i, f);
+            if (i <= 255) {
+                Assert.assertEquals("/1/00/00/" + i ,path);
+            } else {
+                Assert.assertEquals("/1/00/01/" + i, path);
+            }
+        }
+
     }
 }
