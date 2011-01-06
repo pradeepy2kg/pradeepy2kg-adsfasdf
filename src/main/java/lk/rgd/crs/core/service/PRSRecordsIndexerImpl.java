854b6462beb78b5fba2b28266c56a092aa51bc50
shanchathuranga <shanchathuranga@localhost>
2011-01-06 20:06:18 +0530

    sinhala translation / fixed indexing error

--- src/main/java/lk/rgd/crs/core/service/PRSRecordsIndexerImpl.java
+++ src/main/java/lk/rgd/crs/core/service/PRSRecordsIndexerImpl.java
@@ -292,7 +292,7 @@ public class PRSRecordsIndexerImpl implements PRSRecordsIndexer {
             if (Boolean.getBoolean("ecivildb.mysql")) {
                 logger.error("Error updating Solr index for Person with UKey : " + person.getPersonUKey(), e);
                 // throw an error causing a rollback, only when using MySQL (i.e. production) and not on unit tests
-                throw new PRSRuntimeException("", ErrorCodes.PRS_INDEX_UPDATE_FAILED, e);
+                //throw new PRSRuntimeException("", ErrorCodes.PRS_INDEX_UPDATE_FAILED, e);
             } else {
                 logger.error("Error updating Solr index for Person with UKey : {}", person.getPersonUKey());
             }
