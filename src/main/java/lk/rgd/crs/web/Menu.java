2f8928957e0de013c5dbb4beb6ac9dd0a4e34a9d
shan <shan@localhost>
2011-01-17 10:47:29 +0530

    permission check for report generation

--- src/main/java/lk/rgd/crs/web/Menu.java
+++ src/main/java/lk/rgd/crs/web/Menu.java
@@ -86,8 +86,8 @@ public class Menu {
         adminLink.put("eprIndexRecords.do", new Link("indexRecords.label", "/ecivil/management/", "eprIndexRecords.do", Permission.INDEX_RECORDS));
         adminLink.put("eprFindRegistrar.do", new Link("search.registrar", "/ecivil/management/", "eprFindRegistrar.do", Permission.SEARCH_REGISTRAR));
         adminLink.put("eprEditPrimaryLocation.do", new Link(null, "/ecivil/management/", "eprEditPrimaryLocation.do", Permission.REGISTRAR_MANAGEMENT));
-        adminLink.put("eprInitCreateReports.do", new Link("report.title", "/ecivil/management/", "eprInitCreateReports.do", Permission.USER_MANAGEMENT));
-        adminLink.put("eprCreateReports.do", new Link(null, "/ecivil/management/", "eprCreateReports.do", Permission.USER_MANAGEMENT));
+        adminLink.put("eprInitCreateReports.do", new Link("report.title", "/ecivil/management/", "eprInitCreateReports.do", Permission.GENERATE_REPORTS));
+        adminLink.put("eprCreateReports.do", new Link(null, "/ecivil/management/", "eprCreateReports.do", Permission.GENERATE_REPORTS));
 
 
         adrAdminLink.put("eprRegistrarsManagment.do", new Link("registrars.managment", "/ecivil/management/", "eprRegistrarsManagment.do", Permission.REGISTRAR_MANAGEMENT));
