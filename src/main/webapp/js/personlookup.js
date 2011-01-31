e0b7244b7cd6d43362f5b2f95f3f87d32d5c0594
chathuranga <chathuranga@localhost>
2011-01-31 16:11:19 +0530

    defect 2324 fixed

--- src/main/webapp/js/personlookup.js
+++ src/main/webapp/js/personlookup.js
@@ -7,14 +7,37 @@ function personLookup(pin, type) {
                 $("textarea#nameOfficial" + type).val(data1.fullNameInOfficialLanguage);
                 $("textarea#name_english_male" + type).val(data1.fullNameInOfficialLanguage);
                 $("textarea#address" + type).val(data1.lastAddress);
-                var maleDOB = data1.dateOfBirth;
-                if (maleDOB != null) {
-                    $("input#dateOfBirth" + type).val(maleDOB);
+                var personDOB = data1.dateOfBirth;
+                if (personDOB != null) {
+                    $("input#dateOfBirth" + type).val(personDOB);
+                    var personAge = calAgeAtLastBirthday(personDOB);
+                    $("input#age" + type).val(personAge);
                 }
                 $("select#race" + type).val(data1.race);
             });
 }
 
+// calculate age at last birth day for given date of birth
+function calAgeAtLastBirthday(personDOB) {
+    var personAge;
+    var today = new Date();
+    var dob = new Date(personDOB);
+          
+    if (today.getMonth() > dob.getMonth()) {
+        personAge = today.getYear() - dob.getYear();
+
+    } else if(today.getMonth() == dob.getMonth()) {
+        if (today.getDate() >= dob.getDate()) {
+            personAge = today.getYear() - dob.getYear();
+        } else {
+            personAge = (today.getYear() - 1) - dob.getYear();
+        }
+    }  else {
+        personAge = (today.getYear() - 1) - dob.getYear();
+    }
+
+    return personAge;
+}
 
 function malePersonLookUp(pin) {
     var id1 = $("input#" + pin).attr("value");
@@ -26,6 +49,8 @@ function malePersonLookUp(pin) {
                 var maleDOB = data1.dateOfBirth;
                 if (maleDOB != null) {
                     $("input#date_of_birth_male").val(maleDOB);
+                    var maleAge = calAgeAtLastBirthday(maleDOB);
+                    $("input#age_at_last_bd_male").val(maleAge);
                 }
                 $("select#raceMaleId").val(data1.race);
             });
@@ -39,9 +64,11 @@ function femalePersonLookUp(pin) {
                 $("textarea#name_official_female").val(data1.fullNameInOfficialLanguage);
                 $("textarea#name_english_female").val(data1.fullNameInEnglishLanguage);
                 $("textarea#address_female_official").val(data1.lastAddress);
-                var maleDOB = data1.dateOfBirth;
-                if (maleDOB != null) {
-                    $("input#date_of_birth_female").val(maleDOB);
+                var femaleDOB = data1.dateOfBirth;
+                if (femaleDOB != null) {
+                    $("input#date_of_birth_female").val(femaleDOB);
+                    var femaleAge = calAgeAtLastBirthday(femaleDOB);
+                    $("input#age_at_last_bd_female").val(femaleAge);
                 }
                 $("select#raceFemaleId").val(data1.race);
             });
