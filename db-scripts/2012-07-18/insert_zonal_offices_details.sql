119a6d9c28a6088a2e59dc9d2fd3c2fac3130346
Duminda <Duminda@localhost>
2012-07-18 16:22:28 +0530

    DB Scripts to add Zonal Offices

---
+++ db-scripts/2012-07-18/insert_zonal_offices_details.sql
@@ -0,0 +1,46 @@
+-- Database: COMMON
+
+-- ZONAL_OFFICES
+
+INSERT INTO ZONAL_OFFICES (siZonalOfficeName, taZonalOfficeName, enZonalOfficeName, siZonalOfficeMailAddress, taZonalOfficeMailAddress, enZonalOfficeMailAddress, zonalOfficeLandPhone, zonalOfficeFax, zonalOfficeEmail, active, createdTimestamp, createdUserId, lastUpdatedTimestamp, lastUpdatedUserId)
+      VALUES ('බස්නාහිර කලාප කාර්යාලය','Western Zonal Office in ta','Western Zonal Office','ෙජිස්ට්‍රාර් ජනරාල් දෙපාර්තමේන්තුව, හයිලෙවල් ප්ලාසා, ඳෙල්කඳ, නුගේගොඩ','Address in ta','Address in en', '0112827120', '0112827120', 'rgdwestern@sltnet.lk', 1, '2012-07-18 13:00:00', 'system', '2012-07-18 13:00:00', 'system');
+INSERT INTO ZONAL_OFFICES(siZonalOfficeName, taZonalOfficeName, enZonalOfficeName, siZonalOfficeMailAddress, taZonalOfficeMailAddress, enZonalOfficeMailAddress, zonalOfficeLandPhone, zonalOfficeFax, zonalOfficeEmail, active, createdTimestamp, createdUserId, lastUpdatedTimestamp, lastUpdatedUserId)
+      VALUES ('මධ්‍යම කලාප කාර්යාලය', 'Central Zonal Office in ta', 'Central Zonal Office', 'ෙජිස්ට්‍රාර් ජනරාල් දෙපාර්තමේන්තුව, ජාතික නිවාස ගොඩනැගිල්ල, මහනුවර', 'Address in ta', 'Address in en', '0812224470', '0812224470', 'argcentral@sltnet.lk', 1, '2012-07-18 13:00:00', 'system', '2012-07-18 13:00:00', 'system' );
+INSERT INTO ZONAL_OFFICES(siZonalOfficeName, taZonalOfficeName, enZonalOfficeName, siZonalOfficeMailAddress, taZonalOfficeMailAddress, enZonalOfficeMailAddress, zonalOfficeLandPhone, zonalOfficeFax, zonalOfficeEmail, active, createdTimestamp, createdUserId, lastUpdatedTimestamp, lastUpdatedUserId)
+      VALUES ('දකුණු කලාප කාර්යාලය', 'Southern Zonal Office in ta', 'Southern Zonal Office', 'ෙජිස්ට්‍රාර් ජනරාල් දෙපාර්තමේන්තුව, අංක 10, පල්ලිය පාර, චීන කොරටුව, ගාල්ල', 'Address in ta', 'Address in en', '0912224581', '0912224581', 'rgsouthernzo@sltnet.lk', 1, '2012-07-18 13:00:00', 'system', '2012-07-18 13:00:00', 'system' );
+INSERT INTO ZONAL_OFFICES(siZonalOfficeName, taZonalOfficeName, enZonalOfficeName, siZonalOfficeMailAddress, taZonalOfficeMailAddress, enZonalOfficeMailAddress, zonalOfficeLandPhone, zonalOfficeFax, zonalOfficeEmail, active, createdTimestamp, createdUserId, lastUpdatedTimestamp, lastUpdatedUserId)
+      VALUES ('උතුරු කලාප කාර්යාලය', 'Northern Zonal Office in ta', 'Northern Zonal Office', 'ෙජිස්ට්‍රාර් ජනරාල් දෙපාර්තමේන්තුව, යාපනය', 'Address in ta', 'Address in en', '0212222475', '0212222475', 'arg.north@sltnet.lk', 1, '2012-07-18 13:00:00', 'system', '2012-07-18 13:00:00', 'system' );
+INSERT INTO ZONAL_OFFICES(siZonalOfficeName, taZonalOfficeName, enZonalOfficeName, siZonalOfficeMailAddress, taZonalOfficeMailAddress, enZonalOfficeMailAddress, zonalOfficeLandPhone, zonalOfficeFax, zonalOfficeEmail, active, createdTimestamp, createdUserId, lastUpdatedTimestamp, lastUpdatedUserId)
+      VALUES ('නැගෙනහිර කලාප කාර්යාලය', 'Eastern Zonal Office in ta', 'Eastern Zonal Office', 'ෙජිස්ට්‍රාර් ජනරාල් දෙපාර්තමේන්තුව, මඩකලපුව', 'Address in ta', 'Address in en', '0652222227', '0652222227', 'rg_ez@sltnet.lk', 1, '2012-07-18 13:00:00', 'system', '2012-07-18 13:00:00', 'system' );
+INSERT INTO ZONAL_OFFICES(siZonalOfficeName, taZonalOfficeName, enZonalOfficeName, siZonalOfficeMailAddress, taZonalOfficeMailAddress, enZonalOfficeMailAddress, zonalOfficeLandPhone, zonalOfficeFax, zonalOfficeEmail, active, createdTimestamp, createdUserId, lastUpdatedTimestamp, lastUpdatedUserId)
+      VALUES ('වයඹ උතුරු මැද කලාප කාර්යාලය', 'Wayamba & North Central Zonal Office in ta', 'Wayamba & North Central Zonal Office', 'රෙජිස්ට්‍රාර් ජනරාල් දෙපාර්තමේන්තුව, වත්හිමි පාර, කුරුණෑගල', 'Address in ta', 'Address in en', '0372228830', '0372228830', 'rgdnwnca@sltnet.lk', 1, '2012-07-18 13:00:00', 'system', '2012-07-18 13:00:00', 'system' );
+INSERT INTO ZONAL_OFFICES(siZonalOfficeName, taZonalOfficeName, enZonalOfficeName, siZonalOfficeMailAddress, taZonalOfficeMailAddress, enZonalOfficeMailAddress, zonalOfficeLandPhone, zonalOfficeFax, zonalOfficeEmail, active, createdTimestamp, createdUserId, lastUpdatedTimestamp, lastUpdatedUserId)
+      VALUES ('ඌව සබරගමු කලාප කාර්යාලය', 'Uwa Sabaragamuwa Zonal Office in ta', 'Uwa Sabaragamuwa Zonal Office', 'ෙජිස්ට්‍රාර් ජනරාල් දෙපාර්තමේන්තුව, නව නගරය, රත්නපුර', 'Address in ta', 'Address in en', '0452222238', '0452222238', 'rgduvas@sltnet.lk', 1, '2012-07-18 13:00:00', 'system', '2012-07-18 13:00:00', 'system' );
+
+-- ZONAL_OFFICE_DISTRICTS
+
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (1, 1);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (1, 2);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (1, 3);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (2, 4);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (2, 5);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (2, 6);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (3, 7);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (3, 8);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (3, 9);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (4, 10);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (4, 11);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (4, 12);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (4, 13);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (4, 14);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (5, 15);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (5, 16);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (5, 17);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (6, 18);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (6, 19);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (6, 20);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (6, 21);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (7, 22);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (7, 23);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (7, 24);
+INSERT INTO ZONAL_OFFICE_DISTRICTS (zonalOfficeUKey, districtUKey) VALUES (7, 25);
