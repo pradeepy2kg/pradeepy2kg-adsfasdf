7b4b324af33da38bb4e51e510943f3b74f8a8544
Duminda <Duminda@localhost>
2012-10-23 11:00:29 +0530

    Fix for printing issue

--- src/main/webapp/js/print.js
+++ src/main/webapp/js/print.js
@@ -11,13 +11,13 @@ function printPage() {
         var windowName = 'Print' + uniqueName.getTime();
         var printWindow = window.open(windowUrl, windowName, 'left=0,top=0,width=200px,height=200px');
 
-        if (navigator.appVersion.indexOf("Win") != -1) {
+        /*if (navigator.appVersion.indexOf("Win") != -1) {
             printWindow.document.write("<head><link rel=\"stylesheet\" type=\"text/css\" href=\"/ecivil/css/windowsFont.css\"/>");
         }
         else if (navigator.appVersion.indexOf("X11") != -1) {
             printWindow.document.write("<head><link rel=\"stylesheet\" type=\"text/css\" href=\"/ecivil/css/ubuntuFont.css\"/>");
-        }
-        printWindow.document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"/ecivil/css/webform.css\"/>");
+        }*/
+        printWindow.document.write("<head><link rel=\"stylesheet\" type=\"text/css\" href=\"/ecivil/css/webform.css\"/>");
         printWindow.document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"/ecivil/css/style.css\"/>");
         printWindow.document.write("<script language=\"JavaScript\" src=\"/ecivil/js/print.js\"><\/script>");
         printWindow.document.write("<link rel=\"stylesheet\" type=\"text/css\" media=\"print\" href=\"/ecivil/css/print.css\"/></head>");
