3b5ff25403ad919a7a7972bbb5bf239da1daa3b6
Duminda <Duminda@localhost>
2013-03-13 16:33:13 +0530

    Add Sinhala Tamil Syntax Checker

---
+++ src/main/webapp/js/sinhalaTamilSyntaxChecker.js
@@ -0,0 +1,270 @@
+/**
+ * Syntax Checker was originally written by Shanmugarajah Sinnathamby.
+ * Modified to use in the ePopulation Registry and Converted to JavaScript by Duminda Dharmakeerthi.
+ */
+
+/**
+ * Check for Sinhala and Tamil syntax errors and return the corrected text without the syntax errors.
+ * @param id ID of the input field that has to be checked for syntax errors.
+ */
+function checkSyntax(id) {
+    var text = $('#'+id).val();
+    var curChar;
+    var charCode;
+    var output = "";
+    var arrChars;
+    var prevConsonant = 0;
+    var outputDone;
+    var prevCharCode = 0;
+    var nextChar;
+    var nextCharCode;
+    var prevprevChar;
+    var prevprevCharCode;
+    var nextnextChar;
+    var nextnextCharCode;
+
+    text = text.replace("අා", "ආ");
+    text = text.replace("අැ", "ඇ");
+    text = text.replace("අෑ", "ඈ");
+    text = text.replace("උෟ", "ඌ");
+    text = text.replace("එ්", "ඒ");
+    text = text.replace("ෙඑ", "ඓ");
+    text = text.replace("ඔ්", "ඕ");
+    text = text.replace("ඔෟ", "ඓ");
+    text = text.replace("ෙඑ", "ඖ");
+    text = text.replace("ේ", "ේ");
+    text = text.replace("ො", "ො");
+    text = text.replace("ෝ", "ෝ");
+    text = text.replace("ෝ", "ෝ");
+    text = text.replace("ෞ", "ෞ");
+    text = text.replace("ொ", "ொ");
+    text = text.replace("ோ", " ோ");
+    text = text.replace("ௌ", "ௌ");
+    text = text.replace("ා්", "ෳ");
+
+    arrChars = text.split('');
+    for (i = 0; i < arrChars.length; i++) {
+        outputDone = 0;
+        curChar = arrChars[i];
+        charCode = curChar.charCodeAt(0);
+
+        if (charCode == 8204) {
+            var isKsha = 0;
+            // check for ksha kuththu character
+            if (prevCharCode == 3021) {
+                // check for next char as sha
+                if (i + 1 < arrChars.length) {
+                    nextChar = arrChars[i + 1];
+                    nextCharCode = nextChar.charCodeAt(0);
+                    // next char is a sha
+                    if (nextCharCode == 2999)
+                        isKsha = 1;
+                }
+            }
+            if (isKsha == 0) {
+                // output = output + '<' + curChar + '>';
+                outputDone = 1;
+            }
+            prevConsonant = 0;
+        } else if (charCode == 8205) {
+            // check for tamil ksha kuththu character
+            var isHanging = 0;
+            if (prevCharCode == 3021) {
+                // check for next char as sha
+                if (i + 1 < arrChars.length) {
+                    nextChar = arrChars[i + 1];
+                    nextCharCode = nextChar.charCodeAt(0);
+                    // next char is a sha
+                    if (nextCharCode == 2999)
+                        isHanging = 1;
+                }
+            }
+            // check for bandhi and rakar and yanse and reph
+            else if (prevCharCode == 3530) {
+                // check for next char as sha
+                if (i + 1 < arrChars.length) {
+                    nextChar = arrChars[i + 1];
+                    nextCharCode = nextChar.charCodeAt(0);
+                    // next char is a sinhala consonant then allow
+                    if (nextCharCode > 3481 & nextCharCode < 3528) {
+                        if (i - 2 > -1) {
+                            prevprevChar = arrChars[i - 2];
+                            prevprevCharCode = prevprevChar.charCodeAt(0);
+                            // prev prev char is a sinhala consonant then allow
+                            if (prevprevCharCode > 3481 & prevprevCharCode < 3528)
+                                isHanging = 1;
+                        }
+                    }
+                }
+                prevConsonant = 0;
+            }
+            // check for touch character
+            else if (prevCharCode > 3481 & prevCharCode < 3528) {
+                // check for next char as sha
+                if (i + 1 < arrChars.length) {
+                    nextChar = arrChars[i + 1];
+                    nextCharCode = nextChar.charCodeAt(0);
+                    // next char is a sinhala alkirima then allow
+                    if (nextCharCode == 3530) {
+                        if (i + 2 < arrChars.length) {
+                            nextChar = arrChars[i + 2];
+                            nextCharCode = nextChar.charCodeAt(0);
+                            // next char is a sinhala consonant then allow
+                            if (nextCharCode > 3481 & nextCharCode < 3528)
+                                isHanging = 1;
+                        }
+                    }
+                }
+            }
+
+            if (isHanging == 0) {
+                // output = output + '<' + curChar + '>';
+                outputDone = 1;
+            }
+            prevConsonant = 0;
+        }else if (charCode > 3529 & charCode < 3572) {
+            // if sinhala vowel modifier not followed by sinhala consonant
+
+            if (prevConsonant != 1) {
+                // if sinhala kombu is accompanying a consonant with al,o modifier or o al modifier or au modifier
+                if (charCode == 3545) {
+                    if (i + 1 < arrChars.length) {
+                        nextChar = arrChars[i + 1];
+                        nextCharCode = nextChar.charCodeAt(0);
+                        // next char is a sinhala consonant then allow
+                        if (nextCharCode > 3481 & nextCharCode < 3528) {
+                            if (i + 2 < arrChars.length) {
+                                nextnextChar = arrChars[i + 2];
+                                nextnextCharCode = nextnextChar.charCodeAt(0);
+
+                                if (nextnextCharCode == 3530) {
+                                    output = output + nextChar + "ේ";
+                                    outputDone = 1;
+                                    i = i + 2;
+                                } else if (nextnextCharCode == 3535) {
+                                    output = output + nextChar + "ො";
+                                    outputDone = 1;
+                                    i = i + 2;
+                                } else if (nextnextCharCode == 3551) {
+                                    output = output + nextChar + "ෞ";
+                                    outputDone = 1;
+                                    i = i + 2;
+                                } else if (nextnextCharCode == 3571) {
+                                    output = output + nextChar + "ෝ";
+                                    outputDone = 1;
+                                    i = i + 2;
+                                } else {
+                                    output = output + nextChar + curChar;
+                                    outputDone = 1;
+                                    i = i + 1;
+                                }
+                            } else {
+                                // exchange the consonant and kombu
+                                output = output + nextChar + curChar;
+                                outputDone = 1;
+                                i = i + 1;
+                            }
+                        }
+                    }
+                }
+
+                if (charCode == 3571) {
+                    // output = output + "<ා><්>";
+                    outputDone = 1;
+                } else if (outputDone != 1) {
+                    // output = output + '<' + curChar + '>';
+                    outputDone = 1;
+                }
+            }
+            prevConsonant = 0;
+        }else if (charCode > 3006 & charCode < 3045) {
+            // if tamil vowel modifier not followed by tamil consonant
+            if (prevConsonant != 2) {
+                if (charCode == 3014) {
+                    if (i + 1 < arrChars.length) {
+                        nextChar = arrChars[i + 1];
+                        nextCharCode = nextChar.charCodeAt(0);
+                        // next char is a Tamil consonant then allow
+                        if (nextCharCode > 2964 & nextCharCode < 3006) {
+                            if (i + 2 < arrChars.length) {
+                                nextnextChar = arrChars[i + 2];
+                                nextnextCharCode = nextnextChar.charCodeAt(0);
+
+                                if (nextnextCharCode == 3006) {
+                                    output = output + nextChar + "ொ";
+                                    outputDone = 1;
+                                    i = i + 2;
+                                } else if (nextnextCharCode == 2995) {
+                                    output = output + nextChar + "ௌ";
+                                    outputDone = 1;
+                                    i = i + 2;
+                                } else {
+                                    output = output + nextChar + curChar;
+                                    outputDone = 1;
+                                    i = i + 1;
+                                }
+                            } else {
+                                // exchange the consonant and kombu
+                                output = output + nextChar + curChar;
+                                outputDone = 1;
+                                i = i + 1;
+                            }
+                        }
+                    }
+                } else if (charCode == 3015) {
+                    if (i + 1 < arrChars.length) {
+                        nextChar = arrChars[i + 1];
+                        nextCharCode = nextChar.charCodeAt(0);
+                        // next char is a Tamil consonant then allow
+                        if (nextCharCode > 2964 & nextCharCode < 3006) {
+                            if (i + 2 < arrChars.length) {
+                                nextnextChar = arrChars[i + 2];
+                                nextnextCharCode = nextnextChar.charCodeAt(0);
+
+                                if (nextnextCharCode == 3006) {
+                                    output = output + nextChar + "ோ";
+                                    outputDone = 1;
+                                    i = i + 2;
+                                } else {
+                                    output = output + nextChar + curChar;
+                                    outputDone = 1;
+                                    i = i + 1;
+                                }
+                            } else {
+                                // exchange the consonant and kombu
+                                output = output + nextChar + curChar;
+                                outputDone = 1;
+                                i = i + 1;
+                            }
+                        }
+                    }
+                }
+                if (outputDone != 1) {
+                    // // output = output + '<' + curChar + '>';
+                    outputDone = 1;
+                }
+            }
+            prevConsonant = 0;
+        } else if (charCode > 3481 & charCode < 3528) {
+            // Sinhala vowels
+            prevConsonant = 1;
+        } else if (charCode > 2964 & charCode < 3002) {
+            // Tamil vowels
+            prevConsonant = 2;
+        }
+
+        prevCharCode = charCode;
+        if (outputDone == 0)
+            output = output + curChar;
+    }
+
+    text = text.replace("ෳ", "");
+    output = output.replace("<<", "<");
+    output = output.replace(">>", ">");
+    if(text.length != output.length){
+        var check = confirm("There is a syntax error in the following text.\n"+ text +"\nDo you want to correct it as follows?\n "+ output);
+        if(check){
+            $('#'+id).val(output);
+        }
+    }
+}
