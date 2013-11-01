9b53b4296dd3150694d7532405ec51a32c38e7d8
Sisiruwan <Sisiruwan@localhost>
2013-11-01 09:05:03 +0530

    dbscripts

--- db-scripts/2013-04-09/alter_adoption_order.sql
+++ db-scripts/2013-04-09/alter_adoption_order.sql
@@ -4,3 +4,7 @@ USE CRS;
 -- Introduce new column to capture comments.
 ALTER TABLE ADOPTION_ORDER ADD COLUMN `comments` longtext;
 ALTER TABLE ADOPTION_ORDER ADD COLUMN `previousAdoptionIdUKey` bigint(20) DEFAULT NULL;
+
+
+-- Add new column for get Application entered Date (2013-10-04)
+ALTER TABLE ADOPTION_ORDER ADD COLUMN `applicationEnteredDate` DATETIME;
